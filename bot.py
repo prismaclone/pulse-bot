3f4b347640ab9d7590539c59dfd371d77c61efb2
prismaclone <wlincolnh@icloud.com>
2026-03-14 19:13:26 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,11 @@
+await bot.change_presence(
+    status=discord.Status.online,
+    activity=discord.Activity(
+        type=discord.ActivityType.watching,
+        name="the server pulse ⚡"
+    )
+)
+
 import discord
 from discord.ext import commands
 import os
